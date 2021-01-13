ad3c48b1f11fd93353828f004af622b8382a11e0
engrceey <ogbondachristian@gmail.com>
2021-01-13 13:06:10 +0100

    add entity to database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using ZurumPark.Entities;
 
 namespace ZurumPark.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<NationalPark> NationalParks {get; set;}
     }
 }
---
+++ Migrations/20210113120158_AddNationalParkToDb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ZurumPark.Migrations
+{
+    public partial class AddNationalParkToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NationalParks",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    State = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Established = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NationalParks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NationalParks");
+        }
+    }
+}
