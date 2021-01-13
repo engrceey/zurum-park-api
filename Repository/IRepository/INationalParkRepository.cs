0e2bc77f0facadb70bbd9987927df8982063fb7a
engrceey <ogbondachristian@gmail.com>
2021-01-13 13:43:27 +0100

    update national park interface

---
+++ Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using ZurumPark.Entities;
+
+namespace ZurumPark.Repository.IRepository
+{
+    public interface INationalParkRepository
+    {
+        ICollection<NationalPark> GetNationalParks();
+        NationalPark GetNationalPark(int nationalParkId);
+        bool NationalParkExists(string name);
+        bool NationalParkExists(int id);
+        bool CreateNationalPark(NationalPark nationalPark);
+        bool UpdateNationalPark(NationalPark nationalPark);
+        bool DeleteNationalPark(NationalPark nationalPark);
+        bool Save();
+    }
+}
